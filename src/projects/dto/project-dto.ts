7379897bcc1dd3986b1c01fc6885532f264de938
iamchapita <alejandrom646@gmail.com>
2023-08-17 00:17:06 -0600

    Cambio de nombre a propiedad

--- src/projects/dto/project-dto.ts
+++ src/projects/dto/project-dto.ts
@@ -5,14 +5,14 @@ import { User } from "src/users/schema/user.schema";
 export interface CreateProjectDto{
     readonly name: string;
     readonly description: string;
-    readonly locatedInFolder: Folder;
+    readonly parentFolder: Folder;
     readonly user: User;
 }
 
 export class UpdateProjectDto{
     readonly name: string;
     readonly description: string;
-    readonly locatedInFolder: Folder;
+    readonly parentFolder: Folder;
     // readonly user: User;
     // readonly files: File[];
     // readonly colaborators: User[];
