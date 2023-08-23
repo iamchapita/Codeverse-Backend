6704236a309e2c19827ae8b88cb0c23113d2ef6f
iamchapita <alejandrom646@gmail.com>
2023-08-23 16:22:38 -0600

    Obtener usuario por uid

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,38 +1,58 @@
-import { Controller, Get, Post, Put, Delete, Body, Param, Res,  } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	Post,
+	Put,
+	Delete,
+	Body,
+	Param,
+	Res,
+} from "@nestjs/common";
 import { User } from "./schema/user.schema";
-import { UsersService } from './users.service';
-import { CreateUserDto, UpdateUserDto, UpdateUserPlanDto } from './dto/user-dto';
+import { UsersService } from "./users.service";
+import {
+	CreateUserDto,
+	UpdateUserDto,
+	UpdateUserPlanDto,
+} from "./dto/user-dto";
 
-@Controller('users')
+@Controller("users")
 export class UsersController {
+	constructor(private usersService: UsersService) {}
 
-    constructor(
-        private usersService: UsersService
-    ){}
+	@Post("/")
+	createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+		return this.usersService.createUser(createUserDto);
+	}
 
-    @Post('/')
-    createUser(@Body() createUserDto: CreateUserDto): Promise<User>{
-        return this.usersService.createUser(createUserDto);
-    }
+	@Get("/:id")
+	getUser(@Param("id") id: string): Promise<User> {
+		return this.usersService.getUserById(id);
+	}
 
-    @Get('/:id')
-    getUser(@Param('id') id: string): Promise<User>{
-        return this.usersService.getUserById(id);
-    }
+	@Get("/uid/:uid")
+	getUserByUid(@Param("uid") uid: string): Promise<User> {
+		return this.usersService.getUserByUid(uid);
+	}
 
-    @Put('/:id')
-    updateUser(@Param('id') id: string, @Body() updateUser: UpdateUserDto): Promise<User>{
-        return this.usersService.updateUserById(id, updateUser);
-    }
+	@Put("/:id")
+	updateUser(
+		@Param("id") id: string,
+		@Body() updateUser: UpdateUserDto
+	): Promise<User> {
+		return this.usersService.updateUserById(id, updateUser);
+	}
 
-    @Put('/:id/plan')
-    updateUserPlan(@Param('id') id: string, @Body() updateUserPlan: UpdateUserPlanDto): Promise<User>{
-        return this.usersService.updateUserPlan(id, updateUserPlan);
-    }
+	@Put("/:id/plan")
+	updateUserPlan(
+		@Param("id") id: string,
+		@Body() updateUserPlan: UpdateUserPlanDto
+	): Promise<User> {
+		return this.usersService.updateUserPlan(id, updateUserPlan);
+	}
 
-    @Delete('/:id')
-    deleteUser(@Param('id')id: string): Promise<User>{
-        return this.usersService.deleteUserById(id)
-    }
-
+	@Delete("/:id")
+	deleteUser(@Param("id") id: string): Promise<User> {
+		return this.usersService.deleteUserById(id);
+	}
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,36 +1,53 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { User } from './schema/user.schema';
-import { Model } from 'mongoose';
-import { CreateUserDto, UpdateUserDto, UpdateUserPlanDto } from './dto/user-dto'
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { User } from "./schema/user.schema";
+import { Model } from "mongoose";
+import {
+	CreateUserDto,
+	UpdateUserDto,
+	UpdateUserPlanDto,
+} from "./dto/user-dto";
 
 @Injectable()
 export class UsersService {
-    constructor(
-        @InjectModel('users')
-        private readonly usersModel:Model<User>
-    ){}
+	constructor(
+		@InjectModel("users")
+		private readonly usersModel: Model<User>
+	) {}
 
-    async createUser(createUserDto: CreateUserDto): Promise<User>{
-        const user = new this.usersModel(createUserDto);
-        await user.save();
-        return user;
-    }
+	async createUser(createUserDto: CreateUserDto): Promise<User> {
+		const user = new this.usersModel(createUserDto);
+		await user.save();
+		return user;
+	}
 
-    async getUserById(id: string): Promise<User>{
-        return this.usersModel.findById(id);
-    }
+	async getUserById(id: string): Promise<User> {
+		return this.usersModel.findById(id);
+	}
 
-    async deleteUserById(id: string): Promise<User>{
-        return this.usersModel.findByIdAndDelete(id);
-    }
+	async getUserByUid(uid: string): Promise<User | null> {
+		return this.usersModel.findOne({ uid });
+	}
 
-    async updateUserById(id: string, updateUserDto: UpdateUserDto): Promise<User>{
-        return this.usersModel.findByIdAndUpdate(id, updateUserDto, {new: true});
-    }
+	async deleteUserById(id: string): Promise<User> {
+		return this.usersModel.findByIdAndDelete(id);
+	}
 
-    async updateUserPlan(id: string, updateUserPlan: UpdateUserPlanDto): Promise<User>{
-        return this.usersModel.findByIdAndUpdate(id, updateUserPlan, {new: true});
-    }
+	async updateUserById(
+		id: string,
+		updateUserDto: UpdateUserDto
+	): Promise<User> {
+		return this.usersModel.findByIdAndUpdate(id, updateUserDto, {
+			new: true,
+		});
+	}
 
+	async updateUserPlan(
+		id: string,
+		updateUserPlan: UpdateUserPlanDto
+	): Promise<User> {
+		return this.usersModel.findByIdAndUpdate(id, updateUserPlan, {
+			new: true,
+		});
+	}
 }
