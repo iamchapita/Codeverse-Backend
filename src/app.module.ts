69927703cdba70be4cf7016c4653fafa9c32e283
faustojuanramon <faustojro.o@gmail.com>
2023-08-23 20:00:03 -0600

    Agregando configuracion a cors y env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,25 +2,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from "@nestjs/config";
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { FilesModule } from './files/files.module';
 import { FoldersModule } from './folders/folders.module';
-import { MongodbConnectionModule } from './mongodb-connection/mongodb-connection.module';
 import { ProjectsModule } from './projects/projects.module';
 import { SnippetsModule } from './snippets/snippets.module';
 import { UsersModule } from './users/users.module';
 
-import { ProjectsController } from './projects/projects.controller';
-
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal:true}),
-    MongodbConnectionModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
+    MongooseModule.forRoot(process.env.MONGO_OP),
     ProjectsModule,
     FilesModule,
     UsersModule,
     SnippetsModule,
-    FoldersModule
+    FoldersModule,
+    ConfigModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.ALLOWED_ORIGINS,
+    methods: ["GET","POST","PUT","DELETE"],
+    credentials: true
+  });
   await app.listen(3000);
 }
 bootstrap();
