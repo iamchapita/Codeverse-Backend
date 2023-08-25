78ad759c9afe78de814a4edd00572587ea02bf1b
iamchapita <alejandrom646@gmail.com>
2023-08-25 15:19:32 -0600

    Borrado de archivos si se borra el proyecto

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,17 +1,21 @@
-import { Module } from '@nestjs/common';
-import { ProjectsService } from './projects.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ProjectSchema } from './schema/project.schema';
-import { ProjectsController } from './projects.controller';
-import { FoldersModule } from 'src/folders/folders.module';
+import { Module } from "@nestjs/common";
+import { ProjectsService } from "./projects.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ProjectSchema } from "./schema/project.schema";
+import { ProjectsController } from "./projects.controller";
+import { FoldersModule } from "src/folders/folders.module";
+import { FilesModule } from "src/files/files.module";
 
 @Module({
-  imports:[
-    MongooseModule.forFeature( [{name:'projects', schema: ProjectSchema}] ),
-    FoldersModule
-  ],
-  providers: [ProjectsService],
-  exports: [ProjectsService],
-  controllers: [ProjectsController]
+	imports: [
+		MongooseModule.forFeature([
+			{ name: "projects", schema: ProjectSchema },
+		]),
+		FoldersModule,
+		FilesModule,
+	],
+	providers: [ProjectsService],
+	exports: [ProjectsService],
+	controllers: [ProjectsController],
 })
 export class ProjectsModule {}
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,84 +1,99 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Project } from './schema/project.schema';
-import { Model } from 'mongoose';
-import { CreateProjectDto, UpdateProjectDto } from './dto/project-dto';
-import { FoldersService } from 'src/folders/folders.service';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Project } from "./schema/project.schema";
+import { Model } from "mongoose";
+import { CreateProjectDto, UpdateProjectDto } from "./dto/project-dto";
+import { FoldersService } from "src/folders/folders.service";
+import { FilesService } from "src/files/files.service";
 
 @Injectable()
 export class ProjectsService {
-    constructor(
-        @InjectModel('projects')
-        private readonly projectsModel: Model<Project>,
-        private folderService: FoldersService
-    ){}
+	constructor(
+		@InjectModel("projects")
+		private readonly projectsModel: Model<Project>,
+		private folderService: FoldersService,
+		private fileService: FilesService
+	) {}
 
-    async createProject(createProjectDto: CreateProjectDto): Promise<Project>{
-        const project = new this.projectsModel(createProjectDto);
-        await project.save();
+	async createProject(createProjectDto: CreateProjectDto): Promise<Project> {
+		const project = new this.projectsModel(createProjectDto);
+		await project.save();
 
-        await this.folderService.addProject(project.parentFolder, project.id);
-        return project;
-    }
+		await this.folderService.addProject(project.parentFolder, project.id);
+		return project;
+	}
 
-    async getProjectById(id: string): Promise<Project>{
-        return this.projectsModel.findById(id);
-    }
+	async getProjectById(id: string): Promise<Project> {
+		return this.projectsModel.findById(id);
+	}
 
-    async updateProject(id: string, updateProjectDto: UpdateProjectDto): Promise<Project>{
-        return this.projectsModel.findByIdAndUpdate(
-            id,
-            {updateProjectDto, modifiedAt: Date.now()},
-            {new: true}
-        )
-    }
+	async updateProject(
+		id: string,
+		updateProjectDto: UpdateProjectDto
+	): Promise<Project> {
+		return this.projectsModel.findByIdAndUpdate(
+			id,
+			{ updateProjectDto, modifiedAt: Date.now() },
+			{ new: true }
+		);
+	}
 
-    async addFile(id: string, fileId: string): Promise<Project>{
-        return this.projectsModel.findByIdAndUpdate(
-            id,
-            {
-                $push: {files: fileId},
-                modifiedAt: Date.now()
-            },
-            {new: true}
-        );
-    }
+	async addFile(id: string, fileId: string): Promise<Project> {
+		return this.projectsModel.findByIdAndUpdate(
+			id,
+			{
+				$push: { files: fileId },
+				modifiedAt: Date.now(),
+			},
+			{ new: true }
+		);
+	}
 
-    async removeFile(id: string, fileId: string): Promise<Project>{
-        return this.projectsModel.findByIdAndUpdate(
-            id,
-            {
-                $pull: {files: fileId},
-                modifiedAt: Date.now()
-            },
-            {new: true}
-        );
-    }
+	async removeFile(id: string, fileId: string): Promise<Project> {
+		return this.projectsModel.findByIdAndUpdate(
+			id,
+			{
+				$pull: { files: fileId },
+				modifiedAt: Date.now(),
+			},
+			{ new: true }
+		);
+	}
 
-    async addColaborator(id: string, userId: string): Promise<Project>{
-        return this.projectsModel.findByIdAndUpdate(
-            id,
-            {
-                $push: {colaborators: userId}
-            },
-            {new: true}
-        )
-    }
+	async addColaborator(id: string, userId: string): Promise<Project> {
+		return this.projectsModel.findByIdAndUpdate(
+			id,
+			{
+				$push: { colaborators: userId },
+			},
+			{ new: true }
+		);
+	}
 
-    async removeColaborator(id: string, userId: string): Promise<Project>{
-        return this.projectsModel.findByIdAndUpdate(
-            id,
-            {
-                $pull: {colaborators: userId}
-            },
-            {new: true}
-        )
-    }
+	async removeColaborator(id: string, userId: string): Promise<Project> {
+		return this.projectsModel.findByIdAndUpdate(
+			id,
+			{
+				$pull: { colaborators: userId },
+			},
+			{ new: true }
+		);
+	}
 
-    async deleteProject(id: string): Promise<Project>{
-        const projectToBeDeleted = await this.getProjectById(id);
+	async deleteProject(id: string): Promise<Project> {
+		const projectToBeDeleted = await this.getProjectById(id);
 
-        await this.folderService.removeProject(projectToBeDeleted.parentFolder, id);
-        return await this.projectsModel.findByIdAndDelete(id);
-    }
+		await this.folderService.removeProject(
+			projectToBeDeleted.parentFolder,
+			id
+		);
+
+		await Promise.all(
+			projectToBeDeleted.files.map(async (file: any) => {
+				await this.fileService.deleteFile(file);
+			})
+		);
+
+		return await this.projectsModel.findByIdAndDelete(id);
+	}
 }
