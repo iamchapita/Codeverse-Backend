f4a549927461579017e808924365cacc4ca47a13
iamchapita <alejandrom646@gmail.com>
2023-08-26 22:41:31 -0600

    Actualización

--- src/snippets/dto/create-snippet-dto.ts
+++ src/snippets/dto/create-snippet-dto.ts
@@ -2,13 +2,13 @@ import { User } from "src/users/schema/user.schema";
 import { Type } from "../../files/schema/file.schema";
 
 export class CreateSnippetDto {
-    readonly type: Type;
-    readonly code: string;
-    readonly parentFolder: string;
-             user: User;
+	readonly type: Type;
+	readonly code: string;
+	readonly parentFolder: string;
+	readonly user: User;
 }
 
-export class UpdateSnippetDTO{
-    readonly code: string;
-    readonly parentFolder: string;
+export class UpdateSnippetDTO {
+	readonly code: string;
+	readonly parentFolder: string;
 }
--- src/snippets/schema/snippet.schema.ts
+++ src/snippets/schema/snippet.schema.ts
@@ -3,26 +3,33 @@ import { Type } from "../../files/schema/file.schema";
 import { User } from "src/users/schema/user.schema";
 import mongoose from "mongoose";
 
-
 @Schema()
-export class Snippet{
-    @Prop()
-    type: Type;
+export class Snippet {
+	@Prop({ required: false })
+	type: Type;
 
-    @Prop()
-    code: string;
+	@Prop()
+	code: string;
 
-    @Prop({type:mongoose.Schema.Types.ObjectId, ref:'users', required: true})
-    user: User;
+	@Prop({
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "users",
+		required: true,
+	})
+	user: User;
 
-    @Prop({type:mongoose.Schema.Types.ObjectId, ref:'folders', required: true})
-    parentFolder: string;
+	@Prop({
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "folders",
+		required: true,
+	})
+	parentFolder: string;
 
-    @Prop({type:Date, default:Date.now})
-    createdAt: Date;
+	@Prop({ type: Date, default: Date.now })
+	createdAt: Date;
 
-    @Prop()
-    modifiedAt: Date;
+	@Prop()
+	modifiedAt: Date;
 }
 
 export const SnippetSchema = SchemaFactory.createForClass(Snippet);
