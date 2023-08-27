ca6fa922e05c0394893c50330a586902b5e7a96a
iamchapita <alejandrom646@gmail.com>
2023-08-27 04:44:00 -0600

    Conexion de server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,32 @@
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
 
-import { FilesModule } from './files/files.module';
-import { FoldersModule } from './folders/folders.module';
-import { ProjectsModule } from './projects/projects.module';
-import { SnippetsModule } from './snippets/snippets.module';
-import { UsersModule } from './users/users.module';
+import { FilesModule } from "./files/files.module";
+import { FoldersModule } from "./folders/folders.module";
+import { ProjectsModule } from "./projects/projects.module";
+import { SnippetsModule } from "./snippets/snippets.module";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true
-    }),
-    MongooseModule.forRoot(process.env.MONGO_OP),
-    ProjectsModule,
-    FilesModule,
-    UsersModule,
-    SnippetsModule,
-    FoldersModule,
-    ConfigModule
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: ".env",
+			isGlobal: true,
+		}),
+		MongooseModule.forRoot(
+			"mongodb+srv://codeadmin:2rOsaRWrvF2sxDbS@codeverse-cluster.ajx9zkm.mongodb.net/codeverse"
+		),
+		ProjectsModule,
+		FilesModule,
+		UsersModule,
+		SnippetsModule,
+		FoldersModule,
+		ConfigModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
