b341f49837da9108dcd0934c3be8d9694dbf80c2
iamchapita <alejandrom646@gmail.com>
2023-08-27 05:28:06 -0600

    Prueba

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
-  await app.listen(parseInt(process.env.PORT) || 3000);
+	const app = await NestFactory.create(AppModule);
+	app.enableCors({
+		origin: "https://codeverse-frontend-eta.vercel.app//",
+		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+		allowedHeaders: "Content-Type, Accept",
+		exposedHeaders: "Custom-Header",
+		optionsSuccessStatus: 200,
+		preflightContinue: false,
+		credentials: true,
+	});
+	await app.listen(3000);
 }
 bootstrap();
